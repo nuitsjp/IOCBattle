23cf29300579f10e2d285edfd57a00a924ff69c6
nuits.jp <nuits.jp@live.jp>
2017-01-10 11:03:36 +0900

    Apply newest Container

--- IocBattle.Benchmark/Tests/StructureMapContainer.cs
+++ IocBattle.Benchmark/Tests/StructureMapContainer.cs
@@ -3,67 +3,68 @@ using StructureMap;
 
 namespace IocBattle.Benchmark.Tests
 {
-	public class StructureMapContainer : IContainer
-	{
-		public string Name
-		{
-			get { return "StructureMap"; }
-		}
+    public class StructureMapContainer : IContainer
+    {
+        Container container = new Container();
+        public string Name
+        {
+            get { return "StructureMap"; }
+        }
 
-		public T Resolve<T>()
-			where T : class
-		{
-			return ObjectFactory.GetInstance<T>();
-		}
+        public T Resolve<T>()
+            where T : class
+        {
+            return container.GetInstance<T>();
+        }
 
-		public void SetupForTransientTest()
-		{
-			ObjectFactory.Container.Dispose();
+        public void SetupForTransientTest()
+        {
+            container.Dispose();
 
-			ObjectFactory.Initialize(x =>
-											 {
-												 x.For<IRepository>().Use<Repository>();
-												 x.For<IAuthenticationService>().Use<AuthenticationService>();
-												 x.For<UserController>().Use<UserController>();
+            container.Configure(x =>
+            {
+                x.For<IRepository>().Use<Repository>();
+                x.For<IAuthenticationService>().Use<AuthenticationService>();
+                x.For<UserController>().Use<UserController>();
 
-												 x.For<IWebService>().Use<WebService>();
-												 x.For<IAuthenticator>().Use<Authenticator>();
-												 x.For<IStockQuote>().Use<StockQuote>();
-												 x.For<IDatabase>().Use<Database>();
-												 x.For<IErrorHandler>().Use<ErrorHandler>();
+                x.For<IWebService>().Use<WebService>();
+                x.For<IAuthenticator>().Use<Authenticator>();
+                x.For<IStockQuote>().Use<StockQuote>();
+                x.For<IDatabase>().Use<Database>();
+                x.For<IErrorHandler>().Use<ErrorHandler>();
 
-												 x.For<IService1>().Use<Service1>();
-												 x.For<IService2>().Use<Service2>();
-												 x.For<IService3>().Use<Service3>();
-												 x.For<IService4>().Use<Service4>();
+                x.For<IService1>().Use<Service1>();
+                x.For<IService2>().Use<Service2>();
+                x.For<IService3>().Use<Service3>();
+                x.For<IService4>().Use<Service4>();
 
-												 x.For<ILogger>().Use<Logger>();
-											 });
-		}
+                x.For<ILogger>().Use<Logger>();
+            });
+        }
 
-		public void SetupForSingletonTest()
-		{
-			ObjectFactory.Container.Dispose();
+        public void SetupForSingletonTest()
+        {
+            container.Dispose();
 
-			ObjectFactory.Initialize(x =>
-			{
-				x.For<IRepository>().Singleton().Use<Repository>();
-				x.For<IAuthenticationService>().Singleton().Use<AuthenticationService>();
-				x.For<UserController>().Singleton().Use<UserController>();
+            container.Configure(x =>
+            {
+                x.For<IRepository>().Singleton().Use<Repository>();
+                x.For<IAuthenticationService>().Singleton().Use<AuthenticationService>();
+                x.For<UserController>().Singleton().Use<UserController>();
 
-				x.For<IWebService>().Singleton().Use<WebService>();
-				x.For<IAuthenticator>().Singleton().Use<Authenticator>();
-				x.For<IStockQuote>().Singleton().Use<StockQuote>();
-				x.For<IDatabase>().Singleton().Use<Database>();
-				x.For<IErrorHandler>().Singleton().Use<ErrorHandler>();
+                x.For<IWebService>().Singleton().Use<WebService>();
+                x.For<IAuthenticator>().Singleton().Use<Authenticator>();
+                x.For<IStockQuote>().Singleton().Use<StockQuote>();
+                x.For<IDatabase>().Singleton().Use<Database>();
+                x.For<IErrorHandler>().Singleton().Use<ErrorHandler>();
 
-				x.For<IService1>().Singleton().Use<Service1>();
-				x.For<IService2>().Singleton().Use<Service2>();
-				x.For<IService3>().Singleton().Use<Service3>();
-				x.For<IService4>().Singleton().Use<Service4>();
+                x.For<IService1>().Singleton().Use<Service1>();
+                x.For<IService2>().Singleton().Use<Service2>();
+                x.For<IService3>().Singleton().Use<Service3>();
+                x.For<IService4>().Singleton().Use<Service4>();
 
-				x.For<ILogger>().Singleton().Use<Logger>();
-			});
-		}
-	}
+                x.For<ILogger>().Singleton().Use<Logger>();
+            });
+        }
+    }
 }
--- IocBattle.Benchmark/Tests/StructureMapNamedContainer.cs
+++ IocBattle.Benchmark/Tests/StructureMapNamedContainer.cs
@@ -1,71 +1,73 @@
-﻿using IocBattle.Benchmark.Models;
+﻿using System.Runtime.InteropServices;
+using IocBattle.Benchmark.Models;
 using StructureMap;
 
 // How do i specify that it should use a specific named instance ? 
 
 namespace IocBattle.Benchmark.Tests
 {
-	public class StructureMapNamedContainerTest : IContainer
-	{
-		public string Name
-		{
-			get { return "StructureMap Named"; }
-		}
+    public class StructureMapNamedContainerTest : IContainer
+    {
+        private readonly Container container = new Container();
+        public string Name
+        {
+            get { return "StructureMap Named"; }
+        }
 
-		public T Resolve<T>()
-			where T : class
-		{
-			return ObjectFactory.GetNamedInstance<T>("WebService");
-		}
+        public T Resolve<T>()
+            where T : class
+        {
+            return container.GetInstance<T>("WebService");
+        }
 
-		public void SetupForTransientTest()
-		{
-			ObjectFactory.Container.Dispose();
+        public void SetupForTransientTest()
+        {
+            container.Dispose();
 
-			ObjectFactory.Initialize(x =>
-											 {
-												 x.For<IRepository>().Use<Repository>();
-												 x.For<IAuthenticationService>().Use<AuthenticationService>();
-												 x.For<UserController>().Use<UserController>();
+            container.Configure(x =>
+            {
+                x.For<IRepository>().Use<Repository>();
+                x.For<IAuthenticationService>().Use<AuthenticationService>();
+                x.For<UserController>().Use<UserController>();
 
-												 x.For<IWebService>().Use<WebService>().Named("WebService");
-												 x.For<IAuthenticator>().Use<Authenticator>().Named("Authenticator");
-												 x.For<IStockQuote>().Use<StockQuote>().Named("StockQuote");
-												 x.For<IDatabase>().Use<Database>().Named("Database");
-												 x.For<IErrorHandler>().Use<ErrorHandler>().Named("ErrorHandler");
+                x.For<IWebService>().Use<WebService>().Named("WebService");
+                x.For<IAuthenticator>().Use<Authenticator>().Named("Authenticator");
+                x.For<IStockQuote>().Use<StockQuote>().Named("StockQuote");
+                x.For<IDatabase>().Use<Database>().Named("Database");
+                x.For<IErrorHandler>().Use<ErrorHandler>().Named("ErrorHandler");
 
-												 x.For<IService1>().Use<Service1>();
-												 x.For<IService2>().Use<Service2>();
-												 x.For<IService3>().Use<Service3>();
-												 x.For<IService4>().Use<Service4>();
+                x.For<IService1>().Use<Service1>();
+                x.For<IService2>().Use<Service2>();
+                x.For<IService3>().Use<Service3>();
+                x.For<IService4>().Use<Service4>();
 
-												 x.For<ILogger>().Use<Logger>().Named("Logger");
-											 });
-		}
+                x.For<ILogger>().Use<Logger>().Named("Logger");
+            });
+        }
 
-		public void SetupForSingletonTest()
-		{
-			ObjectFactory.Container.Dispose();
+        public void SetupForSingletonTest()
+        {
+            container.Dispose();
 
-			ObjectFactory.Initialize(x =>
-			{
-				x.For<IRepository>().Singleton().Use<Repository>();
-				x.For<IAuthenticationService>().Singleton().Use<AuthenticationService>();
-				x.For<UserController>().Singleton().Use<UserController>();
+            container.Configure(x =>
+            {
+                x.For<IRepository>().Singleton().Use<Repository>();
+                x.For<IAuthenticationService>().Singleton().Use<AuthenticationService>();
+                x.For<UserController>().Singleton().Use<UserController>();
 
-				x.For<IWebService>().Singleton().Use<WebService>().Named("WebService");
-				x.For<IAuthenticator>().Singleton().Use<Authenticator>().Named("Authenticator");
-				x.For<IStockQuote>().Singleton().Use<StockQuote>().Named("StockQuote");
-				x.For<IDatabase>().Singleton().Use<Database>().Named("Database");
-				x.For<IErrorHandler>().Singleton().Use<ErrorHandler>().Named("ErrorHandler");
+                x.For<IWebService>().Singleton().Use<WebService>().Named("WebService");
+                x.For<IAuthenticator>().Singleton().Use<Authenticator>().Named("Authenticator");
+                x.For<IStockQuote>().Singleton().Use<StockQuote>().Named("StockQuote");
+                x.For<IDatabase>().Singleton().Use<Database>().Named("Database");
+                x.For<IErrorHandler>().Singleton().Use<ErrorHandler>().Named("ErrorHandler");
 
-				x.For<IService1>().Singleton().Use<Service1>();
-				x.For<IService2>().Singleton().Use<Service2>();
-				x.For<IService3>().Singleton().Use<Service3>();
-				x.For<IService4>().Singleton().Use<Service4>();
+                x.For<IService1>().Singleton().Use<Service1>();
+                x.For<IService2>().Singleton().Use<Service2>();
+                x.For<IService3>().Singleton().Use<Service3>();
+                x.For<IService4>().Singleton().Use<Service4>();
 
-				x.For<ILogger>().Singleton().Use<Logger>().Named("Logger");
-			});
-		}
-	}
+                x.For<ILogger>().Singleton().Use<Logger>().Named("Logger");
+            });
+        }
+    }
 }
